fc2b0e18a472d180f0b29fb3359c449eaa128e4a
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2024-10-15 01:41:17 -0500

    feat: Configuring Body Parser.

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import express from "express";
+import bodyParser from "body-parser";
 import http from "http";
 import { initSocketServer } from "./utils/index.js";
 
 const app = express();
 const server = http.createServer(app);
 initSocketServer(server);
+
+//Configure Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 export { server };
