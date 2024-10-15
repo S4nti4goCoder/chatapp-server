635c202d2021019424408437ef546c9b07e9a84c
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2024-10-15 01:36:04 -0500

    feat: Connecting to a MongoDB database.

--- constants.js
+++ constants.js
@@ -1,3 +1,7 @@
+export const DB_USER = "youngg";
+export const DB_PASSWORD = "Kriegsmarine04";
+export const DB_HOST = "chatapp.wdfdq.mongodb.net"
+
 export const IP_SERVER = "localhost";
 
 export const PORT = process.env.PORT || 3977;
--- index.js
+++ index.js
@@ -1,26 +1,33 @@
+import mongoose from "mongoose";
 import { server } from "./app.js";
-import { IP_SERVER, PORT } from "./constants.js";
+import { IP_SERVER, PORT, DB_USER, DB_PASSWORD, DB_HOST } from "./constants.js";
 import { io } from "./utils/index.js";
 
-server.listen(PORT, () => {
-  console.log("###########################");
-  console.log("######### API REST ########");
-  console.log("###########################");
-  console.log(`http://${IP_SERVER}/:${PORT}/api`);
+const mongoDbUrl = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/`;
+mongoose
+  .connect(mongoDbUrl)
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log("####################");
+      console.log("##### API REST #####");
+      console.log("####################");
+      console.log(`https://${IP_SERVER}/:${PORT}/api`);
 
-  io.sockets.on("connection", (socket) => {
-    console.log("NUEVO USUARIO CONECTADO");
+      io.sockets.on("connection", (socket) => {
+        console.log("NUEVO USUARIO CONECTADO");
 
-    socket.on("disconnect", () => {
-      console.log("USUARIO DESCONECTADO");
+        socket.on("disconnect", () => {
+          console.log("USUARIO DESCONECTADO");
+        });
+
+        socket.on("subscribe", (room) => {
+          socket.join(room);
+        });
+
+        socket.on("unsubscribe", (room) => {
+          socket.leave(room);
+        });
+      });
     });
-
-    socket.on("subscribe", (room) => {
-      socket.join(room);
-    });
-
-    socket.on("unsubscribe", (room) => {
-      socket.leave(room);
-    });
-  });
-});
+  })
+  .catch((error) => console.log(error));
