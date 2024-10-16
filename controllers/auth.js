e25b2b86c1cfce406bd2ac58a56d8eb540e1f73f
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2024-10-16 16:24:00 -0500

    feat: The endpoint for login is created.

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,17 +10,41 @@ function register(req, res) {
   const salt = bscrypt.genSaltSync(10);
   const hashPassword = bscrypt.hashSync(password, salt);
   user.password = hashPassword;
-  
+
   user
     .save()
     .then((userStorage) => {
       res.status(201).send(userStorage);
     })
-    .catch((err) => {
+    .catch((error) => {
       res.status(400).send({ msg: "Error al registrar el usuario" });
     });
 }
 
+async function login(req, res) {
+  const { email, password } = req.body;
+  const emailLowerCase = email.toLowerCase();
+
+  try {
+    const userStorage = await User.findOne({ email: emailLowerCase });
+
+    if (!userStorage) {
+      return res.status(500).send({ msg: "Error del servidor" });
+    }
+
+    const check = await bscrypt.compare(password, userStorage.password);
+
+    if (!check) {
+      return res.status(400).send({ msg: "Contraseña incorrecta" });
+    } else {
+      return res.status(200).send(userStorage);
+    }
+  } catch (error) {
+    res.status(500).send({ msg: "Error del servidor" });
+  }
+}
+
 export const AuthController = {
   register,
+  login,
 };
