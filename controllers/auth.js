4eaedb63ba15964f46d79131b463dd6aa2b1cd48
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2024-10-16 15:46:42 -0500

    feat: The user registration endpoint is created.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,26 @@
+import bscrypt from "bcryptjs";
 import { User } from "../models/index.js";
 
-export const AuthController = {};
+function register(req, res) {
+  const { email, password } = req.body;
+  const user = new User({
+    email: email.toLowerCase(),
+  });
+
+  const salt = bscrypt.genSaltSync(10);
+  const hashPassword = bscrypt.hashSync(password, salt);
+  user.password = hashPassword;
+  
+  user
+    .save()
+    .then((userStorage) => {
+      res.status(201).send(userStorage);
+    })
+    .catch((err) => {
+      res.status(400).send({ msg: "Error al registrar el usuario" });
+    });
+}
+
+export const AuthController = {
+  register,
+};
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,6 @@ import { AuthController } from "../controllers/index.js";
 
 const api = express.Router();
 
-//Define endpoints...
+api.post("/auth/register", AuthController.register);
 
 export const authRoutes = api;
