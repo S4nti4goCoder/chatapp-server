d44ad72557f59b86fc7a483ce9f4b00d4a162e51
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2024-10-22 02:53:53 -0500

    feat: Creation of a group is done.

--- controllers/group.js
+++ controllers/group.js
@@ -1,5 +1,30 @@
-import {Group} from "../models/index.js"
+import { Group } from "../models/index.js";
+import { getFilePath } from "../utils/index.js";
+
+function create(req, res) {
+  const { user_id } = req.user;
+  const group = new Group(req.body);
+  group.creator = user_id;
+  group.participants = JSON.parse(req.body.participants);
+  group.participants = [...group.participants, user_id];
+
+  if (req.files.image) {
+    const imagePath = getFilePath(req.files.image);
+    group.image = imagePath;
+  }
+  group
+    .save()
+    .then((groupStorage) => {
+      if (!groupStorage) {
+        return res.status(400).send({ msg: "Error al crear el grupo" });
+      }
+      res.status(201).send(groupStorage);
+    })
+    .catch((error) => {
+      res.status(400).send({ msg: "Error del servidor", error });
+    });
+}
 
 export const GroupController = {
-    
-}
+  create,
+};
